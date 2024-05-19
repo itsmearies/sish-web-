8726b57ee11a8082c4a603afa6d306fc7dcfd0f5
itsmearies <jomelmalvar522@gmail.com>
2024-05-19 11:02:17 +0800

    update doctors

--- js/image_marquee.js
+++ js/image_marquee.js
@@ -13,6 +13,20 @@ const images = [
 
     "./assets/images/doctors/otyhopedics/Dr. Frederick Hipol.jpg",
     "./assets/images/doctors/pediaratics/Dr. Cristina Magno.jpg",
+
+    "./assets/images/doctors/internal_medicine/Dr. Francisco Villanueva Jr.jpg",
+    "./assets/images/doctors/internal_medicine/Dr. Gladys Maribal Diaz.jpg",
+    "./assets/images/doctors/internal_medicine/Dr. Maria Ailsa Escobin.jpg",
+    "./assets/images/doctors/internal_medicine/Dr. Michael Perez.jpg",
+
+    "./assets/images/doctors/obstetrics_gynecology/Dr. Antoinette Añonuevo.jpg",
+    "./assets/images/doctors/obstetrics_gynecology/Dr. Catherine Costa.jpg",
+    "./assets/images/doctors/obstetrics_gynecology/Dr. Cynthia Correo.jpg",
+
+    "./assets/images/doctors/opthalmologist/Dr. Relito Saquilayan.jpg",
+
+    "./assets/images/doctors/otyhopedics/Dr. Frederick Hipol.jpg",
+    "./assets/images/doctors/pediaratics/Dr. Cristina Magno.jpg",
     
 ];
 
