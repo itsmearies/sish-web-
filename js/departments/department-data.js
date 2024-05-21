0ca55c4394160af52725fff45401604a53f49251
John Cyrus <johncyrus.tolosa@cvsu.edu.ph>
2024-05-21 13:28:27 +0800

    updated clinical department data

--- js/departments/department-data.js
+++ js/departments/department-data.js
@@ -4,13 +4,23 @@ const departments = [
     description: "Internal medicine physicians are specialists who apply scientific knowledge and clinical expertise to the diagnosis, treatment, and compassionate care of adults across the spectrum from health to complex illness. They are especially well trained in the diagnosis of puzzling medical problems, in the ongoing care of chronic illnesses, and in caring for patients with more than one disease. Internal medicine physicians also specialize in health promotion and disease prevention.",
     services: "Lorem Ipsum"},
 
-    {id: "2", name: "Laboratory", 
+    {id: "2", name: "Obstetrics and Gynecology", 
     logo: "./assets/images/flask.jpg", image: "./assets/images/medical-services.jpg", 
     description: "1",
     services: "Lorem Ipsum"},
 
-    {id: "13", name: "Radiology", 
+    {id: "3", name: "Ophthalmology", 
     logo: "./assets/images/flask.jpg", image: "./assets/images/medical-services.jpg", 
     description: "2",
+    services: "Lorem Ipsum"},
+
+    {id: "4", name: "Otyhopedics", 
+    logo: "./assets/images/flask.jpg", image: "./assets/images/medical-services.jpg", 
+    description: "3",
+    services: "Lorem Ipsum"},
+
+    {id: "5", name: "Pediatrics", 
+    logo: "./assets/images/flask.jpg", image: "./assets/images/medical-services.jpg", 
+    description: "4",
     services: "Lorem Ipsum"}
 ];
